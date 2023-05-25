f13cbe2f128f9ef09bb9f91fa78bf636740913d1
Artx999 <54852665+Artx999@users.noreply.github.com>
2023-05-26 00:34:52 +0200

    Changed function for db credentials to a variable
    Running a function for retrieving database credentials (some from environment variables) every query could be unnecessary.

--- database.py
+++ database.py
@@ -4,17 +4,16 @@ import datetime
 import os
 
 
-def db():
-    host = os.environ['POSTGRES_HOST']
-    port = 5432
-    user = os.environ['POSTGRES_USER']
-    password = os.environ['POSTGRES_PASSWORD']
-    database = os.environ['POSTGRES_DB']
-    return "dbname={} user={} password={} host={} port={}".format(database, user, password, host, port)
+_host = os.environ['POSTGRES_HOST']
+_port = 5432
+_user = os.environ['POSTGRES_USER']
+_password = os.environ['POSTGRES_PASSWORD']
+_database = os.environ['POSTGRES_DB']
+db = "dbname={} user={} password={} host={} port={}".format(_database, _user, _password, _host, _port)
 
 
 def list_users():
-    _conn = psycopg2.connect(db())
+    _conn = psycopg2.connect(db)
     _c = _conn.cursor()
 
     _c.execute("SELECT id FROM users;")
@@ -26,7 +25,7 @@ def list_users():
 
 
 def verify(id, pw):
-    _conn = psycopg2.connect(db())
+    _conn = psycopg2.connect(db)
     _c = _conn.cursor()
 
     _c.execute("SELECT pw FROM users WHERE id = '" + id + "';")
@@ -38,14 +37,14 @@ def verify(id, pw):
 
 
 def delete_user_from_db(id):
-    _conn = psycopg2.connect(db())
+    _conn = psycopg2.connect(db)
     _c = _conn.cursor()
     _c.execute("DELETE FROM users WHERE id = '" + id + "';")
     _conn.commit()
     _conn.close()
 
     # when we delete a user FROM database USERS, we also need to delete all his or her notes data FROM database NOTES
-    _conn = psycopg2.connect(db())
+    _conn = psycopg2.connect(db)
     _c = _conn.cursor()
     _c.execute("DELETE FROM notes WHERE owner = '" + id + "';")
     _conn.commit()
@@ -54,7 +53,7 @@ def delete_user_from_db(id):
     # when we delete a user FROM database USERS, we also need to 
     # [1] delete all his or her images FROM image pool (done in app.py)
     # [2] delete all his or her images records FROM database IMAGES
-    _conn = psycopg2.connect(db())
+    _conn = psycopg2.connect(db)
     _c = _conn.cursor()
     _c.execute("DELETE FROM images WHERE owner = '" + id + "';")
     _conn.commit()
@@ -62,7 +61,7 @@ def delete_user_from_db(id):
 
 
 def add_user(id, pw):
-    _conn = psycopg2.connect(db())
+    _conn = psycopg2.connect(db)
     _c = _conn.cursor()
 
     _c.execute("INSERT INTO users values(%s, %s)", (id.upper(), hashlib.sha256(pw.encode()).hexdigest()))
@@ -72,7 +71,7 @@ def add_user(id, pw):
 
 
 def read_note_from_db(id):
-    _conn = psycopg2.connect(db())
+    _conn = psycopg2.connect(db)
     _c = _conn.cursor()
 
     command = "SELECT note_id, timestamp, note FROM notes WHERE owner = '" + id.upper() + "';"
@@ -87,7 +86,7 @@ def read_note_from_db(id):
 
 def match_user_id_with_note_id(note_id):
     # Given the note id, confirm if the current user is the owner of the note which is being operated.
-    _conn = psycopg2.connect(db())
+    _conn = psycopg2.connect(db)
     _c = _conn.cursor()
 
     command = "SELECT owner FROM notes WHERE note_id = '" + note_id + "';"
@@ -101,7 +100,7 @@ def match_user_id_with_note_id(note_id):
 
 
 def write_note_into_db(id, note_to_write):
-    _conn = psycopg2.connect(db())
+    _conn = psycopg2.connect(db)
     _c = _conn.cursor()
 
     current_timestamp = str(datetime.datetime.now())
@@ -114,7 +113,7 @@ def write_note_into_db(id, note_to_write):
 
 
 def delete_note_from_db(note_id):
-    _conn = psycopg2.connect(db())
+    _conn = psycopg2.connect(db)
     _c = _conn.cursor()
 
     command = "DELETE FROM notes WHERE note_id = '" + note_id + "';"
@@ -125,7 +124,7 @@ def delete_note_from_db(note_id):
 
 
 def image_upload_record(uid, owner, image_name, timestamp):
-    _conn = psycopg2.connect(db())
+    _conn = psycopg2.connect(db)
     _c = _conn.cursor()
 
     _c.execute("INSERT INTO images VALUES (%s, %s, %s, %s)", (uid, owner, image_name, timestamp))
@@ -135,7 +134,7 @@ def image_upload_record(uid, owner, image_name, timestamp):
 
 
 def list_images_for_user(owner):
-    _conn = psycopg2.connect(db())
+    _conn = psycopg2.connect(db)
     _c = _conn.cursor()
 
     command = "SELECT uid, timestamp, name FROM images WHERE owner = '{0}'".format(owner)
@@ -150,7 +149,7 @@ def list_images_for_user(owner):
 
 def match_user_id_with_image_uid(image_uid):
     # Given the note id, confirm if the current user is the owner of the note which is being operated.
-    _conn = psycopg2.connect(db())
+    _conn = psycopg2.connect(db)
     _c = _conn.cursor()
 
     command = "SELECT owner FROM images WHERE uid = '" + image_uid + "';"
@@ -164,7 +163,7 @@ def match_user_id_with_image_uid(image_uid):
 
 
 def delete_image_from_db(image_uid):
-    _conn = psycopg2.connect(db())
+    _conn = psycopg2.connect(db)
     _c = _conn.cursor()
 
     command = "DELETE FROM images WHERE uid = '" + image_uid + "';"
